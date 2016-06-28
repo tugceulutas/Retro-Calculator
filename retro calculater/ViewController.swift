82f4357c1acd824fad7f79258f1f92e0ed3a0971
tuğçe <tugceulutass@gmail.com>
2016-06-28 17:10:30 +0300

    some addition

--- retro calculater/ViewController.swift
+++ retro calculater/ViewController.swift
@@ -7,19 +7,23 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    @IBOutlet weak var outputlbl : UILabel!
+    var btnSound:AVPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path =
+            NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+            }
+    
+
+    @IBAction func numberPressed (btn:UIButton!){
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
